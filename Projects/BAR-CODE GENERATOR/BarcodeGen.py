4090fbff2785e45739c25cbc833e8fdb422b8cec
Shikher Jain <shikherjain786@gmail.com>
2024-12-11 15:53:09 +0530

    BarCode

---
+++ Projects/BAR-CODE GENERATOR/BarcodeGen.py
@@ -0,0 +1,9 @@
+# BAR-CODE GENERATOR
+
+import qrcode as qr
+
+img = qr.make("https://github.com/Shikher-jain/SJ_MAIN")  # jo bhi chaoye uska text ya url
+
+img.save("Advance-Python-SJ/Projects/BAR-CODE GENERATOR/SJ_GitHub.png")
+
+
